1ecee7596660651e6d750dc19ce7e3e99a94f712
jebin007 <jevinsubedi@gmail.com>
2017-09-11 22:33:57 -0500

    Saving New Cards with POST:(Front-end)

--- scrumboard/static/scrumboard/scrumboard.js
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -7,14 +7,20 @@
         
         $scope.add = function (list, title) {
             var card = {
+                list: list.id,
                 title: title
             };
-
-            list.cards.push(card);
+            $http.post('/scrumboard/cards/', card)
+            .then(function(response){
+                list.cards.push(response.data);
+            },
+            function(){
+                alert('Could Not Create The Card!!');
+            });
         };
 
         $scope.data = [];
-        $http.get('/scrumboard/lists').then(function(response) {
+        $http.get('/scrumboard/lists/').then(function(response) {
             $scope.data = response.data;
         });
 
